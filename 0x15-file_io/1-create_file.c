15fde7a2781285abaf13b62e70e9521d853c34db
modusfas <modupefasunon@gmail.com>
2023-06-13 05:31:48 -0700

    message

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,32 +7,26 @@
  *
  * Return: 1 if it success. -1 if it fails.
  */
-int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)       
 {
-	int f;
-	int nletters;
-	int rwr;
+	int fd, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	if (f == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(f, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(f);
+	close(fd);
 
 	return (1);
 }
