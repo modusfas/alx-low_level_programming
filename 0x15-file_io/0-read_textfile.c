9f7a7e0a33b8305bfb28e1a6e3451e25e4431394
modusfas <modupefasunon@gmail.com>
2023-07-10 14:58:12 -0700

    message

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,28 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
- *
- * Return: actual number of letters that can be read and printed
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fid;
-	ssize_t lenx, leny;
-	char *buffer;
+	char *buf;
+	ssize_t fid;
+	ssize_t v;
+	ssize_t y;
 
-	if (filename == NULL)
-		return (0);
-	fid = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fid == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fid);
-		return (0);
-	}
-	lenx = read(fid, buffer, letters);
+	buf = malloc(sizeof(char) * letters);
+	y = read(fid, buf, letters);
+	v = write(STDOUT_FILENO, buf, y);
+
+	free(buf);
 	close(fid);
-	if (lenr == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	leny = write(STDOUT_FILENO, buffer, lenx);
-	free(buffer);
-	if (lenx != leny)
-		return (0);
-	return (leny);
+	return (v);
 }
