abe158532910f3697ea4eb1ca17fabe3e1a959a8
modusfas <modupefasunon@gmail.com>
2023-08-03 10:49:54 -0700

    message

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,27 +2,29 @@
 /**
   * binary_to_uint - function that converts a binary number to an unsigned int.
   * @b: binary number
-  * Return: the converted number or 0 if it fails
+  * Return: the converted number or 0 if
+  * there is one or more chars in the string b that is not 0 or 1
+  * b is NULL
   */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int uint, place;
-	int y;
+	int n;
 
 	if (!b)
 		return (0);
 
-	for (y = 0; b[y]; y++)
+	for (n = 0; b[n]; n++)
 		;
 	n--;
-	for (place = 1, uint = 0; y >= 0; y--)
+	for (place = 1, uint = 0; n >= 0; n--)
 	{
-		if (b[y] == '0')
+		if (b[n] == '0')
 		{
 			place *= 2;
 			continue;
 		}
-		else if (b[y] == '1')
+		else if (b[n] == '1')
 		{
 			uint += place;
 			place *= 2;
